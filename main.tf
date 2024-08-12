82df3a781e081f99b387148e56d2f8e3c2c49f7a
evgenii.kasyan <evgenii.kasyan@maxpay.com>
2024-08-12 18:45:26 +0300

    init

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "https://github.com/den-vasyliev/tf-google-gke-cluster.git"
+  source         = "git::https://github.com/den-vasyliev/tf-google-gke-cluster.git"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = var.GKE_NUM_NODES
